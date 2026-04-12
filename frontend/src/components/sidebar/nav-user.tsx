4723b6ad2551ff2fd36d97bd1a6fb90edb725a76
NguyenMinhTruong <minhtruong7735@gmail.com>
2026-04-13 00:41:19 +0700

    Accept and Decline Friend

--- frontend/src/components/sidebar/nav-user.tsx
+++ frontend/src/components/sidebar/nav-user.tsx
@@ -24,74 +24,86 @@ import type { User } from "@/types/user"
 import { CaretUpDownIcon, SparkleIcon, CheckCircleIcon, CreditCardIcon, BellIcon, SignOutIcon } from "@phosphor-icons/react"
 import { Bell, UserIcon } from "lucide-react"
 import Logout from "../auth/Logout"
+import { useState } from "react"
+import FriendRequestDialog from "../friendRequest/FriendRequestDialog"
 
 export function NavUser({
   user,
 }: {
   user: User;
 }) {
-  const { isMobile } = useSidebar()
+  const { isMobile } = useSidebar();
+  const [friendRequestOpen, setfriendRequestOpen] = useState(false);
 
   return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatarUrl} alt={user.displayName} />
-                <AvatarFallback className="rounded-lg">{user.displayName.charAt(0)}</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.displayName}</span>
-                <span className="truncate text-xs">{user.username}</span>
-              </div>
-              <CaretUpDownIcon className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+    <>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatarUrl} alt={user.username} />
+                  <AvatarImage src={user.avatarUrl} alt={user.displayName} />
                   <AvatarFallback className="rounded-lg">{user.displayName.charAt(0)}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.displayName}</span>
                   <span className="truncate text-xs">{user.username}</span>
                 </div>
-              </div>
-            </DropdownMenuLabel> 
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <UserIcon
-                 className="text-muted-foreground dark:group-focus:!text-accent-foreground"
-                />
-                Tài khoản
+                <CaretUpDownIcon className="ml-auto size-4" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="end"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.avatarUrl} alt={user.username} />
+                    <AvatarFallback className="rounded-lg">{user.displayName.charAt(0)}</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">{user.displayName}</span>
+                    <span className="truncate text-xs">{user.username}</span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <UserIcon
+                    className="text-muted-foreground dark:group-focus:!text-accent-foreground"
+                  />
+                  Tài khoản
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setfriendRequestOpen(true)}
+                >
+                  <Bell
+                    className="text-muted-foreground dark:group-focus:!text-accent-foreground"
+                  />
+                  Thông báo
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer" variant="destructive">
+                <Logout />
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell
-                  className="text-muted-foreground dark:group-focus:!text-accent-foreground"
-                />
-                Thông báo
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer" variant="destructive">
-              <Logout />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+
+      <FriendRequestDialog
+        open={friendRequestOpen}
+        setOpen={setfriendRequestOpen}
+      />
+    </>
   )
 }
